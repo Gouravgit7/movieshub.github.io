aa85908258acdea251c9f67ae97e838992d44a7f
Gouravgit7 <imgouravgoyal0007@gmail.com>
2022-05-25 14:23:01 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Container } from '@mui/material';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, HashRouter } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
 import SimpleBottomNavigation from './components/Mainnav/MainNav';
@@ -11,22 +11,22 @@ import Trending from './Pages/Trending/Trending';
 
 function App() {
   return (
-    <BrowserRouter>
-    <Header />
-    <div className="app">
+    <HashRouter>
+      <Header />
+      <div className="app">
 
-    <Container>
-      <Routes>
-        <Route path='/' element={<Trending />} />
-        <Route path='/movies' element={<Movies/>} />
-        <Route path='/series' element={<Series />} />
-        <Route path='/search' element={<Search />} />
-        <Route path='/details' element={<Details />} />
-      </Routes>
-    </Container>
-    </div>
-   <SimpleBottomNavigation />
-    </BrowserRouter>
+        <Container>
+          <Routes>
+            <Route path='/' element={<Trending />} />
+            <Route path='/movies' element={<Movies />} />
+            <Route path='/series' element={<Series />} />
+            <Route path='/search' element={<Search />} />
+            <Route path='/details' element={<Details />} />
+          </Routes>
+        </Container>
+      </div>
+      <SimpleBottomNavigation />
+    </HashRouter>
   );
 }
 
